966bb1d7ed315b1daee601428045544ca3894918
Matthew DeVoe <matthew.devoe@gmail.com>
2021-02-27 18:57:16 -0700

    Wired router into index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import { itemsRouter } from "./items/items.router";
 
 dotenv.config();
 
@@ -28,6 +29,7 @@ if (!process.env.PORT) {
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use("/items", itemsRouter);
 
 /**
  * Server Activation
