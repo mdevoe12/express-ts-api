df9e96e95853b27397c91e121b0530e0df888a22
Matthew DeVoe <matthew.devoe@gmail.com>
2021-02-27 18:42:42 -0700

    created skeleton for router

---
+++ src/items/items.router.ts
@@ -0,0 +1,21 @@
+/**
+ * Required External Modules and Interfaces
+ */
+
+/**
+ * Router Definition
+ */
+
+/**
+ * Controller Definitions
+ */
+
+// GET items/
+
+// GET items/:id
+
+// POST items/
+
+// PUT items/
+
+// DELETE items/:id
